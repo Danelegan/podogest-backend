d968080815baa98702204a69800aa5d45e3bed80
Danelegan <danielalejandro061204@gmail.com>
2026-09-19 05:36:05 -0300

    feat: configure django admin for appointments

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Appointment
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "patient_name",
+        "rut",
+        "appointment_date",
+        "appointment_time",
+        "service_type",
+    )
+    search_fields = ("patient_name", "rut")
+    list_filter = ("appointment_date", "service_type")
